8bd2c6ed402a92757759899021f40418c9f7e8cc
chevismart <chevismart@gmail.com>
2014-09-02 00:03:29 +0800

    Try get ip list back from server via AJAX

--- src/main/resources/js/gamecenter.js
+++ src/main/resources/js/gamecenter.js
@@ -1,1 +1,23 @@
-//GameCenter JavaScript Start!
+function getClientList(groupId) {
+
+	$.ajax({
+		type: "GET",
+		url: "http://localhost:8080",
+		data: {
+			id: 123
+		},
+		dataType: "jsonp",
+		jsonp: "jsonpCallback",
+		success: function(data) {
+			var ipList = data.ip;
+			$.each(ipList,
+			function(index, content) {
+				alert(content);
+			});
+		},
+		error: function() {
+			alert('fail');
+		}
+
+	});
+}
